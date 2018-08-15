f61fb575b654fed8d82ff513335357c2608a4e4d
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-08-15 13:07:54 -0300

    Update settings.py

--- MonitorDjangoAPI/settings.py
+++ MonitorDjangoAPI/settings.py
@@ -102,10 +102,10 @@ DATABASES = {
     }
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
-}
+#REST_FRAMEWORK = {
+#    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#    'PAGE_SIZE': 20,
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
